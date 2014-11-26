c8c350a4cfb871a69cc109936b4974c69abb4536
uiruson <uiruson@gmail.com>
2014-11-26 23:40:02 +0000

    Added upload image feature

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -9,6 +9,8 @@ class Movie < ActiveRecord::Base
   validates :release_date, presence: true
   validate :release_date_is_in_the_future
 
+  mount_uploader :poster_image_url, AvatarUploader
+
   def review_average
     reviews.sum(:rating_out_of_ten)/reviews.size if reviews.size > 0
   end
